74e1f2a3e7ca901ddcd87f8e0c2d1e202d424b80
Alexander Komarov <Alexplayer2002@gmail.com>
2023-01-30 22:59:55 +0200

    added a base for future telegram bit

---
+++ src/telegram_bot.py
@@ -0,0 +1,23 @@
+"""
+Module representing telegram bot
+"""
+import os
+import telebot
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+# if this exception raised then a programmer forgot to expotr his token which means that program can't contuinue working
+if BOT_TOKEN is  None:
+    raise Exception
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
