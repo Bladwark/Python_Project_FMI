53ee3da751e88688cae48213567def471b31a762
Alexander Komarov <Alexplayer2002@gmail.com>
2023-02-01 14:37:27 +0200

    added function that returns tickets by given trip characteristics and added is_valid_date function's body

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 """
 Module containg usefull functions for operating with the app
 """
-# from __future__ import annotations
+from __future__ import annotations
 from typing import Dict, List
+from trip import Trip
+from functionality import get_cheapest_flights
 # for future me: get time from return and arival and calcuclate it to 
 # local time at the city u start your trip from
 
-def modify_response_date(date: str) -> str:
+def _modify_response_date(date: str) -> str:
     """
     Removes time stamp from returned date of flight
     """
@@ -20,11 +22,11 @@ def make_beautiful_response(city_from: str ,data: Dict[str, Dict[str, Dict[str,s
     result: List[Dict[str,str]] = []
     for city, flights in data.items():
         for flight in flights.values():
-            flight["departure_at"] = modify_response_date(flight["departure_at"])
-            flight["return_at"] = modify_response_date(flight["return_at"])
-            flight["expires_at"] = modify_response_date(flight["expires_at"])
+            flight["departure_at"] = _modify_response_date(flight["departure_at"])
+            flight["return_at"] = _modify_response_date(flight["return_at"])
+            flight["expires_at"] = _modify_response_date(flight["expires_at"])
             city_dict = dict(city_from = city_from,city_to = city)
-            temp_result = f"{city_dict['city_from']}-{city_dict['city_to']} | from {flight['departure_at']} to {flight['return_at']} | avaliable until {flight['expires_at']} | {flight['price']} EUR"
+            # temp_result = f"{city_dict['city_from']}-{city_dict['city_to']} | from {flight['departure_at']} to {flight['return_at']} | avaliable until {flight['expires_at']} | {flight['price']} EUR"
             # result.append(temp_result)
             result.append(dict(city_dict, **flight))
 
@@ -32,3 +34,22 @@ def make_beautiful_response(city_from: str ,data: Dict[str, Dict[str, Dict[str,s
     answer = map ((lambda x: f"{x['city_from']}-{x['city_to']} | from {x['departure_at']} to {x['return_at']} | avaliable until {x['expires_at']} | {x['price']} EUR" ), result)
     
     return list(answer)
+
+def get_tickets(depart_date: str, return_date: str, destination_from: str, destination_to: str, currency: str):
+    """
+    Returns list of tickets by givent trip data
+    """
+    if currency != "-":
+        my_trip = Trip (depart_date, return_date, destination_from, destination_to, currency)
+    my_trip = Trip (depart_date, return_date, destination_from, destination_to)
+    flights = get_cheapest_flights(my_trip)
+    
+    #TODO: if flights is Empty == no such tickets, waht to do 
+    #TODO: if API response is not valid, waht to do 
+    if flights is None:
+        return "some kind of error"
+    
+    return make_beautiful_response(my_trip.destination_from ,flights)
+
+def is_valid_date(date: str) -> bool | None:
+    return True
